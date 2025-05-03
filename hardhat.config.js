ac6bd24fae6d222830f28384b4edf8da883617db
Hng2725 <nguyenviethung270205@gmail.com>
2025-05-03 20:36:52 +0700

    thay doi url

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.28",
   networks: {
-    sepolia: {
-      url: process.env.SEPOLIA_RPC_URL,
+    arbitrumSepolia: {
+      url: process.env.ARBITRUM_SEPOLIA_RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
     },
   },
--- scripts/transferToken.js
+++ scripts/transferToken.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
-
 const { Web3 } = require("web3");
 
-const web3 = new Web3("https://1rpc.io/arb");
+//Dùng endpoint Arbitrum Sepolia
+const web3 = new Web3("https://sepolia-rollup.arbitrum.io/rpc");
 
 const senderPrivateKey = process.env.PRIVATE_KEY;
-const senderAddress = "0x6Ac1314fF3ddF9D94F2c4393b6E0eE1Cd246A7C4"; // Địa chỉ ví người gửi
-const receiverAddress = "0xb128B78b8F075f0e7c70e74209270Bc173F7dEF5"; // Địa chỉ ví người nhận
-const tokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // Địa chỉ Token ERC20
+const senderAddress = "0x6Ac1314fF3ddF9D94F2c4393b6E0eE1Cd246A7C4";
+const receiverAddress = "0xb128B78b8F075f0e7c70e74209270Bc173F7dEF5";
+const tokenAddress = "0x97FB6Dc89E80a3e8C7A690Fe0a298CAC5e0550cA";
 
 const abi = [
   {
@@ -23,25 +23,42 @@ const abi = [
 ];
 
 async function transferToken(amount) {
-  const tokenContract = new web3.eth.Contract(abi, tokenAddress);
-  const amountInWei = web3.utils.toWei(amount.toString(), "ether");
-
-  const tx = {
-    from: senderAddress,
-    to: tokenAddress,
-    gas: 200000,
-    data: tokenContract.methods
+  try {
+    const tokenContract = new web3.eth.Contract(abi, tokenAddress);
+    const amountInWei = web3.utils.toWei(amount.toString(), "ether");
+    const data = tokenContract.methods
       .transfer(receiverAddress, amountInWei)
-      .encodeABI(),
-  };
+      .encodeABI();
 
-  const signedTx = await web3.eth.accounts.signTransaction(
-    tx,
-    senderPrivateKey
-  );
+    const nonce = await web3.eth.getTransactionCount(senderAddress, "latest");
+    const gas = await tokenContract.methods
+      .transfer(receiverAddress, amountInWei)
+      .estimateGas({ from: senderAddress });
 
-  const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-  console.log("Transaction Hash:", receipt.transactionHash);
+    const gasPrice = await web3.eth.getGasPrice();
+
+    const tx = {
+      from: senderAddress,
+      to: tokenAddress,
+      nonce: nonce,
+      gas: gas,
+      gasPrice: gasPrice,
+      data: data,
+    };
+
+    // Ký giao dịch
+    const signedTx = await web3.eth.accounts.signTransaction(
+      tx,
+      senderPrivateKey
+    );
+
+    const receipt = await web3.eth.sendSignedTransaction(
+      signedTx.rawTransaction
+    );
+    console.log("Thành công:", receipt.transactionHash);
+  } catch (err) {
+    console.error("Error:", err.message || err);
+  }
 }
 
 transferToken(10); // Chuyển 10 token
